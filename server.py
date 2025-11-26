b1bc560375cbb418759f49e499753832812606f5
Tom Hammel <tom.hammel@web.de>
2025-11-26 15:02:10 +0100

    Incorporate aas-test-engines for server testing with Neo4jObjectStore

---
+++ server.py
@@ -0,0 +1,13 @@
+from basyx.aas.adapter.http import WSGIApp
+from basyx.aas.adapter.aasx import DictSupplementaryFileContainer
+from aas_mapping.aas_neo4j_adapter.neo_aas_object_store import Neo4jObjectStore
+from aas_mapping.aas_neo4j_adapter.aas_neo4j_client import AASNeo4JClient
+
+client = AASNeo4JClient(uri="bolt://localhost:7687", user="neo4j", password="12345678")
+store = Neo4jObjectStore(client=client)
+application = WSGIApp(object_store=store, file_store=DictSupplementaryFileContainer())
+
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+    run_simple("localhost", 4001, application)
+
