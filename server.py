80db6c8377f9289cba46e4e8555ee0070ae837fe
Tom Hammel <tom.hammel@web.de>
2025-12-03 16:47:53 +0100

    Update Neo4j client URI to use hostname instead of localhost

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from basyx.aas.adapter.aasx import AASXReader
 from aas_mapping.aas_neo4j_adapter.neo_aas_object_store import Neo4jObjectStore
 from aas_mapping.aas_neo4j_adapter.aas_neo4j_client import AASNeo4JClient
 
-client = AASNeo4JClient(uri="bolt://localhost:7687", user="neo4j", password="12345678")
+client = AASNeo4JClient(uri="bolt://neo4j:7687", user="neo4j", password="12345678")
 obj_store = Neo4jObjectStore(client=client)
 file_store = DictSupplementaryFileContainer()
 
