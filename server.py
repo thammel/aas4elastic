7bdcbfeffe64f2e79a8b8200cc618658fab86b7a
Tom Hammel <tom.hammel@web.de>
2025-12-03 15:33:06 +0100

    Update port configuration and add file load to server initialization

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
+import pathlib
+
 from basyx.aas.adapter.http import WSGIApp
 from basyx.aas.adapter.aasx import DictSupplementaryFileContainer
+from basyx.aas.adapter.aasx import AASXReader
 from aas_mapping.aas_neo4j_adapter.neo_aas_object_store import Neo4jObjectStore
 from aas_mapping.aas_neo4j_adapter.aas_neo4j_client import AASNeo4JClient
 
 client = AASNeo4JClient(uri="bolt://localhost:7687", user="neo4j", password="12345678")
-store = Neo4jObjectStore(client=client)
-application = WSGIApp(object_store=store, file_store=DictSupplementaryFileContainer())
+obj_store = Neo4jObjectStore(client=client)
+file_store = DictSupplementaryFileContainer()
+
+for file in pathlib.Path("/input").glob("*.aasx"):
+    with AASXReader(file) as reader:
+        reader.read_into(object_store=obj_store, file_store=file_store)
+application = WSGIApp(object_store=obj_store, file_store=file_store)
 
 if __name__ == "__main__":
     from werkzeug.serving import run_simple
