d90cd4325527e5c7b2e2b4b022a6f89698310819
Tom Hammel <tom.hammel@web.de>
2025-12-03 14:52:54 +0100

    Fix server hostname

--- server.py
+++ server.py
@@ -9,5 +9,5 @@ application = WSGIApp(object_store=store, file_store=DictSupplementaryFileContai
 
 if __name__ == "__main__":
     from werkzeug.serving import run_simple
-    run_simple("localhost", 80, application)
+    run_simple("0.0.0.0", 80, application)
 
